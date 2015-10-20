b9db129e34089d0478a4d1d590a87efe19a57910
Hao Duan <haoduan@outlook.com>
2015-10-21 11:31:06 +1300

    update class Player

--- src/blackjack/model/Player.java
+++ src/blackjack/model/Player.java
@@ -17,7 +17,8 @@ public class Player
     private int win;
     private int lose;
     private int push;
-    private ArrayList<ArrayList<Integer>> inHand;
+    private ArrayList<Integer> handOne;
+    private ArrayList<Integer> handTwo;
     private boolean isInsured;
     private boolean[] isDoubled;
     private boolean isSplit;
@@ -34,7 +35,8 @@ public class Player
         this.isDoubled[0] = false;
         this.isDoubled[1] = false;
         this.isSplit = false;
-        this.inHand = new ArrayList<ArrayList<Integer>>();
+        this.handOne = new ArrayList<>();
+        this.handTwo = new ArrayList<>();
     }
     
     public Player(String name, int chip)
@@ -49,7 +51,8 @@ public class Player
         this.isDoubled[0] = false;
         this.isDoubled[1] = false;
         this.isSplit = false;
-        this.inHand = new ArrayList<ArrayList<Integer>>();
+        this.handOne = new ArrayList<>();
+        this.handTwo = new ArrayList<>();
     }
     
     public Player()
@@ -64,13 +67,8 @@ public class Player
         this.isDoubled[0] = false;
         this.isDoubled[1] = false;
         this.isSplit = false;
-        this.inHand = new ArrayList<ArrayList<Integer>>();
-    }
-    
-    public String toString()
-    {
-        return "\nName: " + this.name + "\nChips: " + this.chip + "\nWin: " + this.win
-                + "\nLose: " + this.lose + "\nPush: " + this.push + "\n";
+        this.handOne = new ArrayList<>();
+        this.handTwo = null;
     }
     
     public String getName()
@@ -78,41 +76,46 @@ public class Player
         return this.name;
     }
     
-    public ArrayList<ArrayList<Integer>> getInHand()
-	{
-		return this.inHand;
-	}
+    public ArrayList<Integer> getHandOne()
+    {
+	return this.handOne;
+    }
+    
+    public ArrayList<Integer> getHandTwo()
+    {
+        return this.handTwo;
+    }
     
     public boolean isDoubled(int whichHand)
-	{
-		return this.isDoubled[whichHand];
-	}
+    {
+	return this.isDoubled[whichHand];
+    }
     
     public boolean isInsured()
-	{
-		return this.isInsured;
-	}
+    {
+	return this.isInsured;
+    }
     
     public boolean isSplit()
-	{
-		return this.isSplit;
-	}
+    {
+	return this.isSplit;
+    }
     
     public int getChip()
-	{
-		return this.chip;
-	}
+    {
+	return this.chip;
+    }
     
     public void setChip(int chip)
-	{
-		this.chip = chip;
-	}
+    {
+	this.chip = chip;
+    }
     
     public void setDoubled(boolean isDoubled)
-	{
-		this.isDoubled[0] = isDoubled;
-		this.isDoubled[1] = isDoubled;
-	}
+    {
+	this.isDoubled[0] = isDoubled;
+	this.isDoubled[1] = isDoubled;
+    }
     
     public void setDoubled(int whichHand, boolean isDoubled)
     {
@@ -120,27 +123,27 @@ public class Player
     }
     
     public void setInsured(boolean isInsured)
-	{
-		this.isInsured = isInsured;
-	}
+    {
+	this.isInsured = isInsured;
+    }
     
     public void setSplit(boolean isSplit)
-	{
-		this.isSplit = isSplit;
-	}
+    {
+	this.isSplit = isSplit;
+    }
     
     public void addWin()
-	{
-		this.win++;
-	}
+    {
+	this.win++;
+    }
     
     public void addLose()
-	{
-		this.lose++;
-	}
+    {
+	this.lose++;
+    }
     
     public void addPush()
-	{
-		this.push++;
-	}
+    {
+	this.push++;
+    }
 }
